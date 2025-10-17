1baa40fc7fcc1015456460d253a7fb05858b3b54
Bluiteng <bluiteng47@gmail.com>
2025-10-17 17:34:19 +0330

    Order Completed

---
+++ Order.cpp
@@ -0,0 +1,105 @@
+﻿#include "Order.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+Order::Order(string name, string lastName, string id, int number)
+{
+    studentName = name;
+    studentLastName = lastName;
+    studentID = id;
+    orderNumber = number;
+    status = "In Progress";
+    itemsHead = nullptr;
+    next = nullptr;
+}
+
+Order::~Order()
+{
+    OrderItem* temp;
+    while (itemsHead)
+    {
+        temp = itemsHead;
+        itemsHead = itemsHead->next;
+        delete temp;
+    }
+}
+
+void Order::addItem(string name, double price)
+{
+    OrderItem* newItem = new OrderItem(name, price);
+    if (!itemsHead)
+    {
+        itemsHead = newItem;
+    }
+    else 
+    {
+        OrderItem* temp = itemsHead;
+        while (temp->next)
+        {
+            temp = temp->next;
+            temp->next = newItem;
+        }
+    }
+}
+
+void Order::removeItem(string name)
+{
+    if (!itemsHead)
+    {
+        return;
+    }
+
+    if (itemsHead->name == name)
+    {
+        OrderItem* temp = itemsHead;
+        itemsHead = itemsHead->next;
+        delete temp;
+        return;
+    }
+
+    OrderItem* temp = itemsHead;
+    while (temp->next && temp->next->name != name)
+    {
+        temp = temp->next;
+    }
+
+    if (temp->next)
+    {
+        OrderItem* toDelete = temp->next;
+        temp->next = toDelete->next;
+        delete toDelete;
+    }
+}
+
+void Order::displayOrder()
+{
+    cout << endl << "Order_Number: " << orderNumber
+        << endl << "Name: " << studentName << " " << studentLastName
+        << endl << "Student Id: " << studentID
+        << endl <<"Status: " << status << endl <<"Items: \n";
+
+    OrderItem* temp = itemsHead;
+    double total = 0;
+    while (temp)
+    {
+        cout << " - " << temp->name << " (" << temp->price << " Rials)" << endl;
+        total += temp->price;
+        temp = temp->next;
+    }
+    cout << " Total Price: " << total << " Rials" << endl;
+}
+
+void Order::saveToFile(ofstream& out)
+{
+    out << studentName << "," << studentLastName << "," << studentID << ","
+        << orderNumber << "," << status;
+
+    OrderItem* temp = itemsHead;
+    while (temp)
+    {
+        out << "," << temp->name << ":" << temp->price;
+        temp = temp->next;
+    }
+    out << endl;
+}
---
+++ Order.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#ifndef ORDER_H
+#define ORDER_H
+
+#include <string>
+#include "OrderItem.h"
+using namespace std;
+
+class Order 
+{
+
+public:
+    string studentName;
+    string studentLastName;
+    string studentID;
+    int orderNumber;
+    string status;
+
+    OrderItem* itemsHead;
+    Order* next;
+
+    Order(string name, string lastName, string id, int number);
+    ~Order();
+
+    void addItem(string name, double price);
+    void removeItem(string name);
+    void displayOrder();
+    void saveToFile(ofstream& out);
+};
+
+#endif
